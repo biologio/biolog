778e252307a9b1b8b9e7401b80d2da1323a028a6
Dipak Yadav <deepakyadav11153@gmail.com>
2015-05-11 22:39:51 +0530

    resolves #15

--- client/views/layouts/basic.js
+++ client/views/layouts/basic.js
@@ -7,5 +7,23 @@ Template.basicLayout.events({
         e.preventDefault();
         Meteor.logout();
         Router.go('home');
+    },
+    'click .main-menu': function(e) {
+        $('.sidebar').sidebar('toggle');
     }
+});
+
+Template.basicLayout.rendered = function(){
+    
+}
+
+Template.basicLayout.helpers({
+    // array of side bar menus
+    sidebarmenus:[
+        {text:"Home", icon:"home"},
+        {text:"Topic", icon:"block layout"},
+        {text:"Friends", icon:"smile"},
+        {text:"History", icon:"calendar"},
+        {text:"Messages", icon:"mail"},
+    ]
 });
