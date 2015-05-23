c2e4a673cba4fcdf28879fc2da6f73341f0f1f94
dpdonohue <dd@daviddonohue.com>
2015-05-23 11:35:10 -0400

    listing meds with icon

--- client/views/meds/meds.js
+++ client/views/meds/meds.js
@@ -1,8 +1,35 @@
 Template['meds'].helpers({
-    items: function() {
+    items: function () {
         if (!getPatient()) return;
         var result = getPatientMeds(getPatient()._id).fetch();
         return result;
+    }
+});
+
+Template.medsItem.rendered = function() {
+
+};
+
+
+Template.medsItem.helpers({
+
+    strengthMg: function() {
+        console.log("StrengthMg: " + JSON.stringify(this));
+        var strengthMg = getValuePath(this, "data");
+        if (!strengthMg) return "?";
+        return strengthMg;
+    },
+
+    timimg: function() {
+        if (this.startDate && this.endDate) {
+            return yyyy_mm_dd(this.startDate) + " to " + yyyy_mm_dd(this.endDate);
+        }
+        if (this.startDate && ! this.endDate) {
+            return "since " + yyyy_mm_dd(this.startDate);
+        }
+        if (! this.startDate && this.endDate) {
+            return "stopped " + yyyy_mm_dd(this.endDate);
+        }
     }
 });
 
