108f352745d18cd80e8a318aada3f48f96034b61
Dipak Yadav <deepakyadav11153@gmail.com>
2015-07-03 09:07:10 +0530

    adding patients

--- server/fixtures/users.js
+++ server/fixtures/users.js
@@ -1,10 +1,12 @@
 Meteor.startup(function () {
-    if(Meteor.users.find().count() < 1000) {
-      _.each(_.range(1000 - Meteor.users.find().count()), function(){
+    var user_to_create = 10;
+    var no_of_users = Meteor.users.find().count();
+    if(no_of_users < user_to_create) {
+      _.each(_.range(user_to_create - no_of_users), function(){
         var randomEmail = faker.internet.email();
         var randomName = faker.name.findName();
         var userName = faker.internet.userName();
-        Accounts.createUser({
+        var user_id = Accounts.createUser({
           username: userName,
           profile: {
             name: randomName,
@@ -12,6 +14,33 @@ Meteor.startup(function () {
           email: randomEmail,
           password: 'password'
         });
+        // console.log(user_id);
+        // var user = Meteor.users.find({_id: user_id});
+        
+        // console.log(user);
+        var patientId = "patient/" + user_id;
+        
+        Meteor.call("getEntity", patientId, function(err, foundPatient) {
+            if (err) {
+                console.error(err);
+            }
+            if (foundPatient) {
+                patient = foundPatient;
+                // setPatient(patient);
+                return patient;
+            }
+            patient = createPatientEntity(patientId, randomName);
+
+            // setPatient(patient);
+            Meteor.call("addEntity", patient);
+
+
+            return patient;
+        });
+        
+        
       });
     }
+    
+   
 });
