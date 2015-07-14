f2b130f1387d182e2b0c67f3b9cb57ed611c0c38
Dipak Yadav <deepakyadav11153@gmail.com>
2015-07-14 08:25:10 +0530

    save entity on profile creation.

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -20,8 +20,43 @@ Template.profile.helpers({
 
 Template.profile.events({
     'click #saveProfile': function(e) {
-       
+        // Update profile
         var data = _.object($("#profileForm").serializeArray().map(function(v) {return [v.name, v.value];} ));
         Meteor.call('updateProfile', data);
+        
+        // create Entity for user.
+        var pt = getPatient();
+        
+        setPatientNickname(pt, Meteor.user().profile.name);
+        setPatientDob(pt, $("#profileDob").val());
+        setPatientSex(pt, $("input:radio[name='profile.sex']").val());
+        setPatientCountry(pt, $("select[name='profile.countryOfResidence']").val());
+        setPatientZip(pt, $("select[name='profile.postalCode']").val());
+        setPatient(pt);
+        
+        
+        
+        var patientId = "patient/" + Meteor.user()._id;
+        Meteor.call("getEntity", patientId, function(err, foundPatient) {
+            if (err) {
+                console.error(err);
+            }
+            if (foundPatient) {
+                patient = foundPatient;
+                setPatient(patient);
+            }
+            patient = createPatientEntity(patientId, Meteor.user().profile.name);
+
+            setPatient(patient);
+            Meteor.call("addEntity", patient);
+
+            
+
+            
+        });
+            
+       
+        
+        
     }
 });
